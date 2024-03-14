21a53c39cbe9fbe9a2f319e0b2e9d8b6c3ad3c73
Durga Prasad <durga.mogatadakala@gmail.com>
2024-03-14 23:05:30 +1100

    Practice Task just to display message in JSON format

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PracticeTask1.Models;
+
+namespace PracticeTask1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return Ok(new Response { Name = "test",Phone="test" });;
+        }
+    }
+}
---
+++ Models/Response.cs
@@ -0,0 +1,8 @@
+﻿namespace PracticeTask1.Models
+{
+    public class Response
+    {
+        public string? Name { get; set; }
+        public string? Phone { get; set; }
+    }
+}
